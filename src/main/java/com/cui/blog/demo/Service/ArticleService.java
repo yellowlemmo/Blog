8b73dcb7b99551c1ad25be24a1d45ca81e3e001e
cuizhichao <czckyd777@163.com>
2018-12-19 22:40:16 +0800

    添加热加载，class文件变化自动重启，首页分页查询博客

--- src/main/java/com/cui/blog/demo/Service/ArticleService.java
+++ src/main/java/com/cui/blog/demo/Service/ArticleService.java
@@ -13,12 +13,32 @@ public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
+    /**
+     * 根据用户id查询博客
+     * @param userId
+     * @param pageable
+     * @return
+     * @throws Exception
+     */
     public Page findallByuser(String userId, Pageable pageable) throws  Exception{
         Page page = articleRepository.findallByuser(userId,pageable);
         return page;
     }
 
+    /**
+     * 保存博客
+     * @param article
+     */
     public void saveBlog(Article article){
         articleRepository.saveAndFlush(article);
+    }
+
+    /**
+     * 分页查询所有博客
+     * @param pageable
+     * @return
+     */
+    public Page findAllBlog(Pageable pageable){
+        return articleRepository.findAll(pageable);
     }
 }
--- src/main/java/com/cui/blog/demo/controller/IndexController.java
+++ src/main/java/com/cui/blog/demo/controller/IndexController.java
@@ -1,18 +1,16 @@
 package com.cui.blog.demo.controller;
 
+import com.cui.blog.demo.Service.ArticleService;
 import com.cui.blog.demo.base.BaseController;
 import com.cui.blog.demo.Service.RoleService;
 import com.cui.blog.demo.Service.UserService;
 import com.cui.blog.demo.pojo.Role;
-import com.cui.blog.demo.pojo.User;
-import com.cui.blog.demo.utils.SpringSecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.SystemPropertyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,18 +20,33 @@ import java.util.List;
 @Controller
 public class IndexController extends BaseController {
 
+    /**
+     * 用户角色service接口
+     */
     @Autowired
     private RoleService roleService;
 
+    /**
+     * 用户service接口
+     */
     @Autowired
     private UserService userService;
 
     @Autowired
     private HttpSession session;
 
+    /**
+     * 博客service接口
+     */
+    @Autowired
+    private ArticleService articleService;
+
 
     @RequestMapping("/index")
     public String index(Model model){
+        PageRequest pageable = PageRequest.of(0,5);
+        Page page = articleService.findAllBlog(pageable);
+        model.addAttribute("page",page);
         return "index";
     }
 
